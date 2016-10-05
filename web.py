6d873baadb7165c110fb1763092470b9c9a8390e
Patrick Huguenin <patrick.huguenin@gmail.com>
2016-10-05 16:31:59 -0400

    Added an about page

--- web.py
+++ web.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
         return render_template("index.html")
+def about():
+        return render_template("about.html")
 
 # debug=True makes it so changes will automatically load
 app.run(debug=True)
